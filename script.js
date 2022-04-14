3cc583b3b1f9ddd5e8eb0c86051bc24392e4a6ae
Leo <lyuholu@gmail.com>
2022-04-14 14:30:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -15,31 +15,27 @@ function calculate(inputData) {
 function infixToPostfix(infix) { // not finished yet
     let postfix = "";
     let stack = ['#'];
-
     const inStackPriority = {
         "^": 1,
         "*": 2, "x": 2, "/": 2,
         "+": 3, "-": 3,
-        "(": 4, "#": 4
+        "(": 4, "#": 4, ")": 4
     };
     const inComingPriority = {
         "(": 0,
         "^": 1,
         "*": 2, "x": 2, "/": 2,
         "+": 3, "-": 3,
-        "#": 4
+        "#": 4, ")": 4
     };
 
     for (let i = 0; i < infix.length; i++) {
         var ch = infix[i];
-        if (ch == "(" || ch == "^") {
+        if (inComingPriority[ch] < inStackPriority[stack[stack.length - 1]]) {
             stack.push(ch);
         }
-        else if (ch == ")") {
-            while (stack.length && stack[stack.length - 1] != "(") {
-                postfix += stack[stack.length - 1];
-                stack.pop();
-            }
+        else {
+            
         }
     }
     return postfix;
@@ -48,9 +44,3 @@ function infixToPostfix(infix) { // not finished yet
 function evaluatePostfix(postfix) { // not finished yet
     return 0;
 }
-
-/* 
-Example:
-inpfix = A/B-C+D*E-A*C
-postfix = AB/C-DE*+AC*-
-*/
